b6f5bf78cbbbdddcc3b2886e93ccedd82892f792
aurel fontaine <ajfame11@gmail.com>
2020-04-03 01:57:27 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,9 +15,10 @@ an attitude of "hot-headed".]
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        #your hashes here!
-      ]
+      :heroine_friends => [a name of "Steven",
+an age of "30", and
+an attitude of "confused".
+]
    }
 }
 
